d9a5963a38b0c3bf2450d65646826bc402a336b9
Marc Becker <marc.h.becker@gmx.de>
2016-01-07 19:46:53 +0100

    improve sundials module object collection
    modules
      use bourne shell builtins to speed up object collection

--- modules/sundials/sundials_collect.sh
+++ modules/sundials/sundials_collect.sh
@@ -10,7 +10,7 @@ fi
 # target type
 [ -z "${LIBRARY_TYPE}" ] && LIBRARY_TYPE="shared"
 
-# elements to include
+# need SOME elements
 if [ $# -lt 1 ]; then \
   printf '%s\n' 'missing SUNDIALS elements' 1>&2
   exit 2
@@ -18,41 +18,59 @@ fi
 
 # printf '%s\n' "collect SUNDIALS objects" 1>&2
 
-# hash matches
-objs=`tempfile --prefix='sun_'`
+# initialize object buffers
+OBJECTS=
+COLLISIONS=
 
-# loop over objects to include
-for d in "$@"; do \
+# loop over sundials modules
+(cd "${DIR_SUNDIALS}/src/" && for d in "$@"; do \
   case "$d" in
     nvec_ser)      s="nvecserial";;
     nvec_openmp)   s="nvecopenmp";;
     nvec_pthreads) s="nvecopenmp";;
     *) s="$d";;
   esac
-  for f in `find "${DIR_SUNDIALS}/src/${d}/CMakeFiles/sundials_${s}_${LIBRARY_TYPE}.dir" -name '*.c.o'`; do \
-# no objects hash file
-    if [ ! -r "${objs}" ]; then \
-      sha256sum "$f" > "${objs}"
-      continue
-    fi
-# find hash for object
-    o=`grep \`basename "$f"\` "${objs}"`
-    if [ -z "$o" ]; then \
-      sha256sum "$f" >> "${objs}"
-      continue
-    fi
-# check for object collisions
-    fhash=`sha256sum "$f" | cut -f 1 -d ' '`
-    ohash=`printf '%s' "$o" | cut -f 1 -d ' '`
-    if [ "x${fhash}" != "x${ohash}" ]; then \
-      printf '%s:\n%s >\n%s  %s\n' 'conflicting objects' "$o" "${fhash}" "$f" 1>&2
-      exit 1
-    fi
+  # loop over module objects
+  for f in `find ${d}/CMakeFiles/sundials_${s}_${LIBRARY_TYPE}.dir -name '*.c.o'`; do \
+    # object name and hash
+    o="${f##*/}"
+    h=
+    ho=
+    hv=
+    for c in ${OBJECTS}; do \
+      # compare object names
+      [ "${o}" != "${c##*/}" ] && continue
+      # full path match
+      [ "${f}" = "${c}" ] && { printf '%s\n' "module '${d}' already processed" 1>&2; exit 1; }
+      # hash on name-only match
+      [ -n "${h}" ] || {
+        h=`sha256sum "${f}"`
+        ho="${h##*/}"
+        hv=${h%% *}
+      }
+      # look for existing collisions
+      for s in $COLLISIONS; do \
+        so=${s%%:*}
+        sv=${s%:*}
+        sv=${sv##*:}
+        # check object name
+        [ ${so} != ${ho} ] && continue
+        # matching hashes, object has name collision entry
+# echo ${sv},${hv} 1>&2
+        [ "${sv}" = "${hv}" ] && { f=; break; }
+        # object hash collision
+        printf '%s: %s %s\n' "collision" "${f}" "${s##*:}"  1>&2
+        exit 2
+      done
+      # add if new name collision found
+      [ -n "${f}" ] && COLLISIONS="${COLLISIONS} ${ho}:${hv}:${f}"
+      f=
+      break
+#       exit 2
+    done
+    [ -n "${f}" ] || continue
+    # add new object
+    OBJECTS="${OBJECTS} ${f}"
+    printf '%s\n' "${DIR_SUNDIALS}/src/${f}"
   done
-done
-
-# output unique objects
-cut -f3 -d ' ' "${objs}"
-
-# remove hash file
-unlink "${objs}"
+done)
