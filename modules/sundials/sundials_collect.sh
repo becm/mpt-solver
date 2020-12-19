e60bb1872eeaf77160f18335ff5f3df07b6b9097
Marc Becker <becm@gmx.de>
2020-12-19 20:54:55 +0100

    simplify SUNDIALS object collection script
    modules
      remove SUNDIALS object duplicate removal and conflict detection
      combine paths rules for SUNDIALS objects to be collected
      improve grouping/naming of SUNDIALS module selection in Makefile

--- modules/sundials/sundials_collect.sh
+++ modules/sundials/sundials_collect.sh
@@ -16,63 +16,16 @@ if [ $# -lt 1 ]; then \
   exit 2
 fi
 
-# printf '%s\n' "collect SUNDIALS objects" 1>&2
-
-# initialize object buffers
-OBJECTS=
-COLLISIONS=
-
 # loop over sundials modules
 (cd "${DIR_SUNDIALS}/src/" && for d in "$@"; do \
   case "$d" in
-    nvector_*)      s="nvec${d#nvector_}";              d="nvector/${d#nvector_}";;
-    sunmatrix_*)    s="sunmatrix${d#sunmatrix_}";       d="sunmatrix/${d#sunmatrix_}";;
-    sunlinsol_*)    s="sunlinsol${d#sunlinsol_}";       d="sunlinsol/${d#sunlinsol_}";;
-    sunnonlinsol_*) s="sunnonlinsol${d#sunnonlinsol_}"; d="sunnonlinsol/${d#sunnonlinsol_}";;
-    generic)        s="generic";                        d="sundials";;
-    *) s="$d";;
+    nvector_*)  s="nvec${d#nvector_}"; d="nvector/${d#nvector_}";;
+    sun*)       s="${d%%_*}${d#*_}";   d="${d%%_*}/${d#*_}";;
+    generic)    s="generic";           d="sundials";;
+    *)          s="$d";;
   esac
   # loop over module objects
   for f in `find ${d}/CMakeFiles/sundials_${s}_obj_${LIBRARY_TYPE}.dir -name '*.c.o'`; do \
-    # object name and hash
-    o="${f##*/}"
-    h=
-    ho=
-    hv=
-    for c in ${OBJECTS}; do \
-      # compare object names
-      [ "${o}" != "${c##*/}" ] && continue
-      # full path match
-      [ "${f}" = "${c}" ] && { printf '%s\n' "module '${d}' already processed" 1>&2; exit 1; }
-      # hash on name-only match
-      [ -n "${h}" ] || {
-        h=`sha256sum "${f}"`
-        ho="${h##*/}"
-        hv=${h%% *}
-      }
-      # look for existing collisions
-      for s in ${COLLISIONS}; do \
-        so=${s%%:*}
-        sv=${s%:*}
-        sv=${sv##*:}
-        # check object name
-        [ ${so} != ${ho} ] && continue
-        # matching hashes, object has name collision entry
-# echo ${sv},${hv} 1>&2
-        [ "${sv}" = "${hv}" ] && { f=; break; }
-        # object hash collision
-        printf '%s: %s %s\n' "collision" "${f}" "${s##*:}"  1>&2
-        exit 2
-      done
-      # add if new name collision found
-      [ -n "${f}" ] && COLLISIONS="${COLLISIONS} ${ho}:${hv}:${f}"
-      f=
-      break
-#       exit 2
-    done
-    [ -n "${f}" ] || continue
-    # add new object
-    OBJECTS="${OBJECTS} ${f}"
     printf '%s\n' "${DIR_SUNDIALS}/src/${f}"
   done
 done)
