2a702d26b2d39cb3dc260e79eb8b9ba855a93253
Marc Becker <becm@gmx.de>
2026-04-08 23:38:33 +0200

    chore(modules): test and bump SUNDIALS to 7.7.0
    update target version in Makefile
    use new option names in CMake cache settings

--- modules/sundials/sundials.cmake
+++ modules/sundials/sundials.cmake
@@ -5,14 +5,14 @@
 set(CMAKE_INSTALL_PREFIX "$ENV{MPT_PREFIX}" CACHE PATH "Top level install directory")
 
 # enable features
-set(ENABLE_PTHREAD ON CACHE BOOL "Enable PTHREAD support")
+set(SUNDIALS_ENABLE_PTHREAD ON CACHE BOOL "Enable PTHREAD support")
 
 # enable and set path for SUNDIALS examples
-set(EXAMPLES_INSTALL ON CACHE BOOL "Enable installation of SUNDIALS examples")
-set(EXAMPLES_INSTALL_PATH "${CMAKE_INSTALL_PREFIX}/examples" CACHE PATH "Target path for SUNDIALS examples")
+set(SUNDIALS_ENABLE_EXAMPLES_INSTALL ON CACHE BOOL "Enable installation of SUNDIALS examples")
+set(SUNDIALS_EXAMPLES_INSTALL_PATH "${CMAKE_INSTALL_PREFIX}/examples" CACHE PATH "Target path for SUNDIALS examples")
 
 # supply library locations for LAPACK/BLAS libraries
-set(ENABLE_LAPACK ON CACHE BOOL "Enable LAPACK backend support")
+set(SUNDIALS_ENABLE_LAPACK ON CACHE BOOL "Enable LAPACK backend support")
 set(LAPACK_LIBRARIES
 	"${CMAKE_INSTALL_PREFIX}/lib/x86_64-linux-gnu/liblapack.so"
 	"${CMAKE_INSTALL_PREFIX}/lib/x86_64-linux-gnu/libblas.so"
